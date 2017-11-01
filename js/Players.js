b018799d0b914b40a4d8e6f478af4586d2989f73
Alber Pedraza <alberpedrazaes@gmail.com>
2017-11-01 18:57:37 +0100

    synchronization, 2D Array and board canvas

--- js/Players.js
+++ js/Players.js
@@ -1,10 +1,13 @@
-function Players(x, y) {
+function Players(x, y, cx, cy,c_area) {
   this.x = x;
   this.y = y;
-  this.vx = 9;
-  this.vy = 8;
-  this.life = 3;
-  this.radius = 20;
+  this.cx = cx;
+  this.cy = cy;
+  this.c_area = c_area;
+  this.vx = 5;
+  this.vy = 5;
+  this.life = 0;
+  this.radius = 30;
   this.color = 'black';
   //this.direction_x;
   //this.direction_y;
@@ -18,48 +21,100 @@ Players.prototype.clearPlayerPos = function(x, y, content) {
 };
 
 //funciones de movimimento
+  //movimiento en Matriz
 Players.prototype.moveUp = function(x, y, content) {
-  this.x--;
-  myBoards.mapItems[x][y] = content;
+    this.x--;
+    myBoards.mapItems[x][y] = content;
+
+    ///movimiento en canvas
+    this.cy -= this.c_area;
+    var that = this;
+    setTimeout(function(cx,cy){
+    console.log("esto es ctx: " + ctx);
+    console.log("setTimeout cx,cy: " + that.cx,that.cy);
+    that.updatePlayerCanvas(that.cx,that.cy);
+  },20);
+//cierra control de inserccion en matriz y movimiento en canvas
+    console.log("moveUp: x,y: " + x,y);
 };
 Players.prototype.moveDown = function(x, y, content) {
   this.x++;
   myBoards.mapItems[x][y] = content;
+  ///movimiento en canvas
+  this.cy += this.c_area;
+  var that = this;
+  setTimeout(function(cx,cy){
+  console.log("esto es ctx: " + ctx);
+  console.log("setTimeout cx,cy: " + that.cx,that.cy);
+  that.updatePlayerCanvas(that.cx,that.cy);
+},20);
+    console.log("moveDown: x,y: " + x,y);
 };
 Players.prototype.moveLeft = function(x, y, content) {
   this.y--;
   myBoards.mapItems[x][y] = content;
+  ///movimiento en canvas
+  this.cx -= this.c_area;
+  var that = this;
+  setTimeout(function(cx,cy){
+  console.log("esto es ctx: " + ctx);
+  console.log("setTimeout cx,cy: " + that.cx,that.cy);
+  that.updatePlayerCanvas(that.cx,that.cy);
+},20);
 };
 Players.prototype.moveRight = function(x, y, content) {
   this.y++;
   myBoards.mapItems[x][y] = content;
+  ///movimiento en canvas
+  this.cx += this.c_area;
+  var that = this;
+  setTimeout(function(cx,cy){
+  console.log("esto es ctx: " + ctx);
+  console.log("setTimeout cx,cy: " + that.cx,that.cy);
+  that.updatePlayerCanvas(that.cx,that.cy);
+},20);
 };
 
-
-
-Players.prototype.lifeCheck = function() {
-
-};
-Players.prototype.paintPlayer = function(ctx) {
-      ctx.beginPath();
-      ctx.arc(this.x+40, this.y+45, this.radius, 0, Math.PI * 2, true);
-      ctx.closePath();
-      ctx.fillStyle = this.color;
-      ctx.fill();
-  };
-Players.prototype.updatePlayer = function (ctx) {
+Players.prototype.updatePlayerCanvas = function (cx,cy) {
+    /*
     ctx.clearRect(0, 0, canvas.width, canvas.height);
     myBoards.start(ctx);
     myPlayers1.paintPlayer(ctx);
+*/
+    console.log("updatePlayerCanvas: " + cx,cy,ctx);
+    //actualizo jugador
+    //this.cx += this.vx;
+    //this.cy += this.vy;
 
-    this.x += this.vx;
-    this.y += this.vy;
-    myPlayers1.paintPlayer(ctx);
-
+/*
     if (this.y + this.vy > canvas.height || this.y + this.vy < 0) {
     this.vy *= -1;
     }
     if (this.x + this.vx > canvas.width || this.x + this.vx < 0) {
     this.vx *= -1;
     }
+*/
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    myBoards.start(ctx);
+    myPlayers1.paintPlayer(ctx);
+
+  };
+
+
+Players.prototype.lifeCheck = function() {
+
+};
+Players.prototype.paintPlayer = function(ctx) {
+      console.log("cx,cy, c_area: ",this.cx, this.cy,this.c_area);
+      ctx.beginPath();
+      //creo jugador
+      ctx.arc(this.cx+50, this.cy+50, this.radius, 0, Math.PI * 2, true);
+      ctx.closePath();
+      ctx.fillStyle = this.color;
+      ctx.fill();
+  };
+
+    Players.prototype.stopPlayer = function(ctx){
+    myBoards.start(ctx);
+    myPlayers1.paintPlayer(ctx);
   };
--- main.js
+++ main.js
@@ -16,7 +16,7 @@ $(document).ready(function() {
 
   myMaps = new Maps();
   myBombs = new Bombs();
-  myPlayers1 = new Players(0, 0);
+  myPlayers1 = new Players(4, 4, 355, 355,89.75);
   myBoards = new Boards(7, 7);
 
   var p_con = "w";
@@ -28,18 +28,17 @@ $(document).ready(function() {
   //añade posicion del jugador al mapa
   myPlayers1.addPlayerMap(myPlayers1.x, myPlayers1.y, p_con);
 
-  //pinta jugador y tablero
+  //pinta tablero y jugador
   myBoards.start(ctx);
   myPlayers1.paintPlayer(ctx,myPlayers1.x, myPlayers1.y);
-  setInterval(myPlayers1.updatePlayer(ctx),60);
+
+
+
+
+
 //////////////////// Controles////////////////////////
 
 var keyPrueba = document.getElementById("map");
-keyPrueba.onkeypress = function(evt) {
-    var charCode = evt.which;
-    var charStr = String.fromCharCode(charCode);
-    alert(charStr);
-};
 
 document.onkeydown = function(ev) {
   var key = ev.keyCode;
@@ -52,24 +51,30 @@ document.onkeydown = function(ev) {
     switch (key) {
       case 38:
         console.log("arriba");
-        console.log(" antes de la funcion", myPlayers1.x);
-        myPlayers1.moveUp(myPlayers1.x, myPlayers1.y, p_con);
+        console.log(" <--funcion x: ", myPlayers1.x);
+        //if(myPlayers1.x >= 0 || myPlayers1.x <= 8 || myPlayers1.y >= 0 || myPlayers1.y <= 8){
+        myPlayers1.moveUp(myPlayers1.x, myPlayers1.y, p_con,ctx);
         myPlayers1.clearPlayerPos(backPos_x, backPos_y, p_con_cls);
-        console.log("despues de la funcion", myPlayers1.x);
+        //};
+        console.log("funcion x -->", myPlayers1.x);
         console.log(myBoards.mapItems);
         break;
       case 40:
         console.log("abajo");
+        console.log(" <--funcion x: ", myPlayers1.x);
+        if(myPlayers1.x >= 0 || myPlayers1.x <= 8 || myPlayers1.y >= 0 || myPlayers1.y <= 8){
         myPlayers1.moveDown(myPlayers1.x, myPlayers1.y, p_con);
         myPlayers1.clearPlayerPos(backPos_x, backPos_y, p_con_cls);
-        console.log("despues de la funcion", myPlayers1.x);
+        };
+
+      console.log("funcion x -->", myPlayers1.x);
         console.log(myBoards.mapItems);
         break;
       case 37:
         console.log("izquierda");
         myPlayers1.moveLeft(myPlayers1.x, myPlayers1.y, p_con);
         myPlayers1.clearPlayerPos(backPos_x, backPos_y, p_con_cls);
-        console.log("despues de la funcion", myPlayers1.y);
+        console.log("funcion -->", myPlayers1.x);
         console.log(myBoards.mapItems);
         break;
       case 39:
@@ -77,10 +82,12 @@ document.onkeydown = function(ev) {
         myPlayers1.moveRight(myPlayers1.x, myPlayers1.y, p_con);
         myPlayers1.clearPlayerPos(backPos_x, backPos_y, p_con_cls);
         console.log("despues de la funcion", myPlayers1.y);
+        console.log("funcion -->", myPlayers1.x);
         console.log(myBoards.mapItems);
         break;
       default:
         console.log("tecla sin movimiento");
+
     }
 
 
